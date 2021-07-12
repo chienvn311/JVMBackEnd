03f1fc431fb90778026355a1347786063752e04a
Chienvn311 <chienvn311@live.com>
2021-07-13 03:27:27 +1000

    Initiate Project

---
+++ src/main/java/com/example/jbe/JbeApplication.java
@@ -0,0 +1,13 @@
+package com.example.jbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JbeApplication.class, args);
+    }
+
+}
